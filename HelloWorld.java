44d8a989446ca8af108c7fe98f6e2597f32e9fc1
aryo <aryofreeyoung@gmail.com>
2019-12-05 12:56:17 +0700

    update 3 file

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,18 @@
 public class HelloWorld{
         public static void main(String args[]){
             System.out.println("Hello Dunia !!");
+			mobil bmw = new mobil();//bangun objek bmw
+			bmw.merk = "BMW 320i";
+			bmw.maju();
         }
+}
+
+
+class mobil{
+	//atribut
+	public String merk;
+	//methode
+	public void maju(){
+		System.out.println("Mobil "+this.merk+" Maju!!! ngeeeng...");
+	}
 }
